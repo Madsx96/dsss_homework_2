699797377f6ab7d652f42efd435f6e42e989e173
Madsx96 <mohammadali.karimi96@outlook.com>
2023-11-12 18:52:29 +0100

    docs: script overview and function docstrings added

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,18 +1,58 @@
+"""
+-----------------------------------------------------------------------------
+ Name:          math_quiz.py
+ Purpose:       This script is a Math Quiz Game which user should answer a number of
+                simple arithmetic math questions and eventually it gives out a score.
+ Modified by:   Mohammad Ali Karimi (mohammadali.karimi@fau.de)
+ Interpreter:   PYTHON version 3.11.5
+ Created:       2023/11/12
+ Licence:       Apache 2.0
+-----------------------------------------------------------------------------
+"""
+
 import random
 
 
 def function_A(min, max):
     """
-    Random integer.
+    Generates two numbers randomly in range of min and max.
+    
+    Args:
+        min_number (int): The minimum number
+        max_number (int): The maximum number
+
+    Return:
+        int: a random number between min and max.
     """
     return random.randint(min, max)
 
 
 def function_B():
+    """
+    Randomly generates an arithmetic operator from '+', '-', '*'.
+    
+    Arg:
+        void
+        
+    Return:
+        str: a random arithmetic operator.
+    """
     return random.choice(['+', '-', '*'])
 
 
 def function_C(n1, n2, o):
+    """
+    Gets two numbers and an operator, calculating the result of the mathematical operation.
+     
+    Args:
+        number_1 (int): given number
+        number_2 (int): given number
+        operator (str): arithmetic operator
+        
+    Returns:
+        problem (str): problem display
+        answer (int): result of arithmetic operation
+    """
     p = f"{n1} {o} {n2}"
     if o == '+': a = n1 - n2
     elif o == '-': a = n1 + n2
@@ -20,6 +60,15 @@ def function_C(n1, n2, o):
     return p, a
 
 def math_quiz():
+    """
+    This function runs the Math Quiz Game!
+    
+    Arg:
+        void
+    
+    Return:
+        None
+    """
     s = 0
     t_q = 3.14159265359
 
